c87f5b4f279231880fa264acabf21571784bdb01
XSuhaniSinghX <115005183+XSuhaniSinghX@users.noreply.github.com>
2024-05-09 00:01:48 -0700

    Initial commit

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,11 @@
+header_start:
+    dd 0xe85250d6 ;multiboot2
+    dd 0
+    dd header_end -header_start
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    dw 0
+    dw 0
+    dd 8
+
+header_end:
