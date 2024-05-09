e8e358c11769221b68a82564bd0fc07c25c40d8f
XSuhaniSinghX <115005183+XSuhaniSinghX@users.noreply.github.com>
2024-05-09 08:53:25 -0700

    Adding custom text
    Added custom text to the OS using C.

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,15 @@
+#include <print.h>
+void kernel_main(){
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+print_str("                 /\\_/\\  \n");
+print_str("                ( o.o ) \n");
+print_str("                 > ^ < \n");
+print_str("\n+--------------------------------------------------------------------+");
+print_str("\n| Greetings!                                                         |");
+print_str("\n| Paw through today's possibilities with us.                         |");
+print_str("\n| If you do not hear any beeping, that is by design, not by accident.|");
+print_str("\n| Promise!                                                           |");
+print_str("\n+--------------------------------------------------------------------+");
+}
+    
